269c16f3a1ba0a33bc6196d02df4c81aec3e091f
devrathi46 <devrathi67155@gmal.com>
2026-06-30 01:16:22 +0530

    minor changes

--- PassBy/PassingObjectReassigning.java
+++ PassBy/PassingObjectReassigning.java
@@ -1,15 +1,11 @@
 package PassBy;
 
 public class PassingObjectReassigning {
-
     public static void change(Student s1){
         s1=new Student("Harsh",25);
     }
-
-
     public static void main(String[] args) {
         Student s1=new Student("Dev",24);
         System.out.println(s1.name);
     }
-    
 }
